6f90dbe38b1201abbef5b23dad025781a3b53ee7
Darkmattou <matthieu.mesn72@sfr.fr>
2017-03-10 16:06:44 +0100

    header

--- index.php
+++ index.php
@@ -2,21 +2,52 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="web/css/style.css">
-	<link rel="stylesheet" type="text/css" href="web/css/bootstrap.min.css">
+
+	<!-- bootstrap css -->
+	<link href="web/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- custom css -->
+	<link rel="stylesheet" href="web/css/style.css">
+
+	<!-- fonts -->
+	<link href="web/css/font-awesome.min.css" rel="stylesheet"">
 	
 	<title>Institut IMIE Le Mans</title>
 </head>
 <body>
 	<div class="container">
-		<div class="row">
-			<div class="col-lg-6">
-				<img class= "logo" src="img/logo-imie_1.png"></img>
+
+		<header>
+
+			<div class="row">
+				<div class="col-lg-10">
+					<img class="logo" src="web/img/logo-imie_1.png">
+				</div>
+				<div class="col-lg-2 telHeader">
+					<span><i class="fa fa-phone fa-custom" aria-hidden="true"></i>06 34 74 20 10</span>		
+				</div>			
 			</div>
-			<div class="col-lg-6">
-				hello
+			<div class="row">
+				<ul class="menu">
+					<li class="color00b6de"><span>Nantes</span></li>
+					<li class="colorfdb515"><span>Angers</span></li>
+					<li class="colored3126"><span>Rennes</span></li>
+					<li class="coloref7a35"><span>Laval</span></li>
+					<li class="color58bf94"><span>Le Mans</span></li>
+					<li class="color00b6de"><span>Caen</span></li>
+					<li class="colorfdb515"><span>Paris</span></li>
+				</ul>																			
+			</div>	
+
+		</header>
+
+		<footer>
+			<div class="row">
+				
 			</div>
-		</div>
+		</footer>
+
 	</div>
+		
 </body>
 </html>
