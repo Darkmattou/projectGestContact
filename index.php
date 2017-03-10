b55bb6c1be58b4ca4742437d39390f6d5b8c7bac
Guigs72 <gcabaret96@laposte.net>
2017-03-10 11:28:14 +0100

    guigs

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="web/css/style.css">
+	<link rel="stylesheet" type="text/css" href="web/css/bootstrap.min.css">
+	
+	<title>Institut IMIE Le Mans</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<img class= "logo" src="img/logo-imie_1.png"></img>
+			</div>
+			<div class="col-lg-6">
+				hello
+			</div>
+		</div>
+	</div>
+</body>
+</html>
