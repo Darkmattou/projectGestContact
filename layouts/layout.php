039a44204092381532a1ba65d5122b56f667d7e8
Guigs72 <gcabaret96@laposte.net>
2017-03-14 15:05:23 +0100

    formulaire

--- layouts/layout.php
+++ layouts/layout.php
@@ -4,13 +4,13 @@
 	<meta charset="utf-8">
 
 	<!-- bootstrap css -->
-	<link href="web/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../web/css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- custom css -->
-	<link rel="stylesheet" href="web/css/style.css">
+	<link rel="stylesheet" href="../web/css/style.css">
 
 	<!-- fonts -->
-	<link href="web/css/font-awesome.min.css" rel="stylesheet"">
+	<link href="../web/css/font-awesome.min.css" rel="stylesheet"">
 	
 	<title>Institut IMIE Le Mans</title>
 </head>
@@ -21,7 +21,7 @@
 
 			<div class="row">
 				<div class="col-lg-10">
-					<img class="logo" src="web/img/logo-imie_1.png">
+					<img class="logo" src="../web/img/logo-imie_1.png">
 				</div>
 				<div class="col-lg-2 telHeader">
 					<span><i class="fa fa-phone fa-custom" aria-hidden="true"></i>02 28 01 37 54</span>		
@@ -46,6 +46,7 @@
 		<div class="content">
 
 		 <?php 
+		 $vueAAfficher="../views/pageVisiteur.php";
 		 include($vueAAfficher); 
 		 ?> 										<!-- Dépend de comment on appelle la variable qui affiche les vues -->
 
@@ -54,7 +55,11 @@
 
 		<footer>
 			<div class="row">
-				
+				<div class="content">
+    <a class="lien-accroche" href="https://imie-ecole-informatique.fr/dates-de-rentrees.html" title=""><img alt="" class="img-accroche" longdesc="https://imie-ecole-informatique.fr/www.imie-ecole-informatique.fr/dates-de-rentrees.html" src="https://imie-ecole-informatique.fr/sites/default/files/imce/accroches/dates_de_rentrees.png" style="height: 260px; width: 380px;"></a>
+<p class="styleh1 red">prochaines rentrées de NOS campus :</p>
+<p>LE DÉTAIL DES PROCHAINES DATES DE RENTRÉES DES FORMATIONS DISPENSÉES À L'IMIE SONT DISPONIBLES</p>
+<br><a class="btn" href="https://imie-ecole-informatique.fr/dates-de-rentrees.html">En savoir plus</a><br>  </div>
 			</div>
 		</footer>
 
--- views/pageVisiteur.php
+++ views/pageVisiteur.php
@@ -1,8 +1,21 @@
-<?php
-$vueAAfficher ="views/pageVisiteur.php";
-?>
 	<div class="row">
 		<div class="col-lg-12">
-			<h2 class ="titre_formulaire">Candidature</h2>
+			<div class="col-lg-4">
+			</div>
+			<div class="col-lg-4">
+				<h2 class ="titre_formulaire">Candidature</h2>
+			</div>
+			<div class="col-lg-4">
+			</div>
 		</div>
 	</div>
+		<div class="row">
+			<div class="cadreForm">
+				<div class="progressbar">
+					<div class="progress-bar progress-bar-striped active" role="progressbar" 
+						aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:50%"> 100%
+  </div>
+				</div>
+			</div>
+		</div>
+		
